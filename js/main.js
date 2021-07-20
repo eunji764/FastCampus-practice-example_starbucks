7b8e45a3d2ae70ccd9f5169491d0183188f15ccc
eunji764 <rladmswl0216@naver.com>
2021-07-21 00:09:50 +0900

    section.visual부분

--- js/main.js
+++ js/main.js
@@ -17,4 +17,36 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 
+const badgeEl = document.querySelector('header .badges');
+// document = html자체, window = 프로젝트가 나타나고있는 브라우저창을 말함 우리가보고있는 화면자체
+window.addEventListener('scroll', _.throttle(function(){
+  if(window.scrollY>500){
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity : 0,
+      display : 'none'
+    });
+  } else {
+    // 배지 보여주기
+    gsap.to(badgeEl, .6, {
+      opacity : 1,
+      display : 'block'
+    });
+  }
+}, 300));
+//_.throttle(함수,시간(밀리세컨단위)) => 함수에 밀리세컨시간만큼 부하를 줘서 함수가 우르르실행되는걸 방지함, 스크롤이벤트와 관련된 작업할때 사용함
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach(function(fadeEl, index){
+  gsap.to(fadeEl, 1, {
+    delay : (index+1) * .7,
+    opacity : 1
+  })
+})
+
+
+
+
 
